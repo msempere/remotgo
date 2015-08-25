9499e012446af30116ae4115ef3a138fb4ecde57
Miguel <msempere@gmx.com>
2015-08-25 18:34:07 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/codegangsta/cli"
+	"github.com/msempere/remotgo/utils"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "remotgo"
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "role",
+			Value: "role",
+			Usage: "instance role",
+		},
+		cli.StringFlag{
+			Name:  "environment",
+			Value: "environment",
+			Usage: "instance environment",
+		},
+		cli.StringFlag{
+			Name:  "username",
+			Value: "username",
+			Usage: "ssh username",
+		},
+		cli.StringFlag{
+			Name:  "password",
+			Value: "",
+			Usage: "ssh password",
+		},
+		cli.StringFlag{
+			Name:  "command",
+			Value: "command",
+			Usage: "command",
+		},
+	}
+	app.Action = func(c *cli.Context) {
+		ins, err := utils.GetInstances()
+
+		if err != nil {
+			panic(err)
+		}
+
+		instances := utils.Filter(ins, utils.CreateFilter(map[string]string{"role": c.String("role"), "environment": c.String("environment")}))
+		for _, instance := range instances {
+			fmt.Println(*instance.PublicDnsName)
+			_, _, result, err := utils.SshExec(*instance.PublicDnsName, c.String("username"), c.String("password"), c.String("command"), 100)
+			if len(err) != 0 {
+				fmt.Println(err)
+			} else {
+				fmt.Println(result)
+			}
+		}
+	}
+	app.Run(os.Args)
+
+}
---
+++ utils/aws.go
@@ -0,0 +1,51 @@
+package utils
+
+import "github.com/aws/aws-sdk-go/service/ec2"
+
+func GetInstances() ([]*ec2.Instance, error) {
+	svc := ec2.New(nil)
+	instances := make([]*ec2.Instance, 0)
+
+	resp, err := svc.DescribeInstances(nil)
+	if err != nil {
+		return nil, err
+	}
+
+	for idx, _ := range resp.Reservations {
+		for _, instance := range resp.Reservations[idx].Instances {
+			instances = append(instances, instance)
+		}
+	}
+	return instances, nil
+}
+
+func Filter(instances []*ec2.Instance, f func(*ec2.Instance) bool) []*ec2.Instance {
+	filtered := make([]*ec2.Instance, 0)
+
+	for _, instance := range instances {
+		if f(instance) {
+			filtered = append(filtered, instance)
+		}
+	}
+	return filtered
+}
+
+func exist(tag *ec2.Tag, tags []*ec2.Tag) bool {
+	for _, t := range tags {
+		if *t.Key == *tag.Key && *t.Value == *tag.Value {
+			return true
+		}
+	}
+	return false
+}
+
+func CreateFilter(m map[string]string) func(inst *ec2.Instance) bool {
+	return func(inst *ec2.Instance) bool {
+		for key, value := range m {
+			if !exist(&ec2.Tag{Key: &key, Value: &value}, inst.Tags) {
+				return false
+			}
+		}
+		return true
+	}
+}
